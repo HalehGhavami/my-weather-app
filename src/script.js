bb56010c28e511feeb0a06608d51d16943823779
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-11-14 22:36:09 +0330

    added unit conversion, icon,speed,humidity
     wind speed, weather description, and weather icon

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-function formatDate(now) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let days = [
     'Sunday',
     'Monday',
@@ -8,7 +9,7 @@ function formatDate(now) {
     'Friday',
     'Saturday',
   ];
-  let date = new Date();
+  let day = days[date.getDay()];
   let min = date.getMinutes();
   if (min < 10) {
     min = `0${min}`;
@@ -17,20 +18,37 @@ function formatDate(now) {
   if (hour < 10) {
     hour = `0${hour}`;
   }
-  return `${days[date.getDay()]} ${hour}:${min}`;
+  return `Last updated: ${day} ${hour}:${min}`;
 }
-let currentDate = document.querySelector('.date');
-
-let date = new Date();
-currentDate.innerHTML = formatDate(date);
 
 function displayWeatherCondition(response) {
-  document.querySelector('#city-title').innerHTML = response.data.name;
-  document.querySelector('#temperature').innerHTML = Math.round(
-    response.data.main.temp
+  document.querySelector('.date').innerHTML = formatDate(
+    response.data.dt * 1000
   );
+
+  document.querySelector('#city-title').innerHTML = response.data.name;
+
   document.querySelector('.description').innerHTML =
     response.data.weather[0].description;
+
+  document.querySelector(
+    '#humidity'
+  ).innerHTML = `Humidity: <span>${response.data.main.humidity}</span> %`;
+
+  document.querySelector(
+    '#wind'
+  ).innerHTML = `Wind: <span>${response.data.wind.speed}</span> km/h`;
+
+  celsiusTemp = response.data.main.temp;
+  let tempElement = document.querySelector('#temperature');
+  tempElement.innerHTML = Math.round(celsiusTemp);
+
+  let iconElement = document.querySelector('#icon');
+  iconElement.setAttribute(
+    'src',
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute('alt', response.data.weather[0].description);
 }
 
 function searchCity(city) {
@@ -49,20 +67,34 @@ function handleSubmit(e) {
   }
 }
 
-function searchLocation(pos) {
-  let apiKey = '6bfa54f242cbb59343d4e58db578dc61';
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-function getCurrentLocation(e) {
+function displayFrenheit(e) {
   e.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+  celsiusLink.classList.add('active');
+  fahrenheitLink.classList.remove('active');
+  let fahrenheitTemp = Math.round((celsiusTemp * 9) / 5 + 32);
+  let tempElement = document.querySelector('#temperature');
+  tempElement.innerHTML = fahrenheitTemp;
 }
+
+function displayCelsius(e) {
+  e.preventDefault();
+  fahrenheitLink.classList.add('active');
+  celsiusLink.classList.remove('active');
+  let tempElement = document.querySelector('#temperature');
+  tempElement.innerHTML = Math.round(celsiusTemp);
+}
+
+//global variables
 
 let formSubmit = document.querySelector('form');
 formSubmit.addEventListener('submit', handleSubmit);
 
-let currentButton = document.querySelector('#current-location-button');
-currentButton.addEventListener('click', getCurrentLocation);
+let fahrenheitLink = document.querySelector('#fahrenheit-link');
+fahrenheitLink.addEventListener('click', displayFrenheit);
+
+let celsiusTemp = null;
+
+let celsiusLink = document.querySelector('#celsius-link');
+celsiusLink.addEventListener('click', displayCelsius);
 
 searchCity('Tehran');
